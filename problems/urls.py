0e75de47d1738f5a7db329dd8440d788ebf7b0b2
mdr <matthew.drummond.ritch@gmail.com>
2024-08-28 13:08:51 -0400

    improve device search

--- problems/urls.py
+++ problems/urls.py
@@ -4,7 +4,7 @@ from problems import views
 
 urlpatterns = [
     # ex: /polls/
-    path("", views.manufacturer_search, name="index"),
+    path("", views.device_search, name="index"),
     path("list_manufacturers", views.list_manufacturers, name="list_manufacturers",),
     path("list_devices", views.list_devices, name="list_devices",),
     path("device_info/<str:mn>", views.device_info, name="device_info",),
