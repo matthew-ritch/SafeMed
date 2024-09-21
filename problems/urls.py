c0a2c92751c9b72cf6910d1907d7f96d608c876d
mdr <matthew.drummond.ritch@gmail.com>
2024-09-21 13:11:13 -0400

    serve sitemap

--- problems/urls.py
+++ problems/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from problems import views
 
@@ -6,5 +7,6 @@ urlpatterns = [
     # ex: /polls/
     path("", views.home, name="index"),
     path("device_info/<str:mn>", views.device_info, name="device_info",),
-    path("device_search", views.device_search, name="device_search",)
+    path("device_search", views.device_search, name="device_search",),
+    path("robots.txt", TemplateView.as_view(template_name="problems/robots.txt", content_type="text/plain")),
 ]
